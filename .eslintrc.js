576ed99059cbdfa6a9924f79c1630ccfacf3f67d
Parthasarathy <you@example.com>
2025-06-22 15:46:02 +0530

    lint disabled

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+};
